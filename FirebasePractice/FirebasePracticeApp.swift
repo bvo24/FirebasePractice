e61ceb50b7c32b107f19562cf7d93a3668999a91
bvo24 <brianvo2020@gmail.com>
2024-06-24 15:49:52 -0400

    Initial Commit

---
+++ FirebasePractice/FirebasePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebasePracticeApp.swift
+//  FirebasePractice
+//
+//  Created by Brian Vo on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebasePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
